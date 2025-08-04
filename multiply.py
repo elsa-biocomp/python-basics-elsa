3374cc47b0a76c233299bca163753dc47e758562
Elsa Mukherjee <224131383+elsa-biocomp@users.noreply.github.com>
2025-08-04 23:16:07 +0530

    multiplication of three float type variables

---
+++ multiply.py
@@ -0,0 +1,5 @@
+a= float(input(" enter value of a :"))
+b= float(input(" enter value of b:"))
+c= float(input(" enter value of c :"))
+d= a*b*c
+print(" the multiplicated value of the three variables is:", d)
